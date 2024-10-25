34bc1d19b7eb333fc4fe443dad2d9f3f75e60bea
ellypeop <ellyprogramming@gmail.com>
2024-10-25 19:48:03 +0100

    Upload PUML, python code and PDFs

---
+++ vqe.py
@@ -0,0 +1,133 @@
+# Cell
+# General imports
+import numpy as np
+
+# Pre-defined ansatz circuit and operator class for Hamiltonian
+from qiskit.circuit.library import EfficientSU2
+from qiskit.quantum_info import SparsePauliOp
+
+# SciPy minimizer routine
+from scipy.optimize import minimize
+
+# Plotting functions
+import matplotlib.pyplot as plt
+
+# Cell
+# runtime imports
+from qiskit_ibm_runtime import QiskitRuntimeService, Session
+from qiskit_ibm_runtime import EstimatorV2 as Estimator
+
+# To run on hardware, select the backend with the fewest number of jobs in the queue
+service = QiskitRuntimeService(channel="ibm_quantum")
+backend = service.least_busy(operational=True, simulator=False)
+
+# Cell
+hamiltonian = SparsePauliOp.from_list(
+    [("YZ", 0.3980), ("ZI", -0.3980), ("ZZ", -0.0113), ("XX", 0.1810)]
+)
+
+# Cell
+ansatz = EfficientSU2(hamiltonian.num_qubits)
+ansatz.decompose().draw("mpl", style="iqp")
+
+# Cell
+num_params = ansatz.num_parameters
+num_params
+
+# Cell
+from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+
+target = backend.target
+pm = generate_preset_pass_manager(target=target, optimization_level=3)
+
+ansatz_isa = pm.run(ansatz)
+
+# Cell
+ansatz_isa.draw(output="mpl", idle_wires=False, style="iqp")
+
+# Cell
+hamiltonian_isa = hamiltonian.apply_layout(layout=ansatz_isa.layout)
+
+# Cell
+def cost_func(params, ansatz, hamiltonian, estimator):
+    """Return estimate of energy from estimator
+
+    Parameters:
+        params (ndarray): Array of ansatz parameters
+        ansatz (QuantumCircuit): Parameterized ansatz circuit
+        hamiltonian (SparsePauliOp): Operator representation of Hamiltonian
+        estimator (EstimatorV2): Estimator primitive instance
+        cost_history_dict: Dictionary for storing intermediate results
+
+    Returns:
+        float: Energy estimate
+    """
+    pub = (ansatz, [hamiltonian], [params])
+    result = estimator.run(pubs=[pub]).result()
+    energy = result[0].data.evs[0]
+
+    cost_history_dict["iters"] += 1
+    cost_history_dict["prev_vector"] = params
+    cost_history_dict["cost_history"].append(energy)
+    print(f"Iters. done: {cost_history_dict['iters']} [Current cost: {energy}]")
+
+    return energy
+
+# Cell
+cost_history_dict = {
+    "prev_vector": None,
+    "iters": 0,
+    "cost_history": [],
+}
+
+# Cell
+x0 = 2 * np.pi * np.random.random(num_params)
+
+# Cell
+x0
+
+# Cell
+with Session(backend=backend) as mode:
+    estimator = Estimator(mode=mode)
+    estimator.options.default_shots = 5000  # Reduce the number of shots to speed up each job
+    
+    options = {
+        'maxiter': 50,  # Example: still keeping max iterations
+        'rhobeg': 0.5,   # Starting value for the trust region radius
+        'tol': 1e-3      # Final accuracy of the optimization
+    }
+
+    res = minimize(
+        cost_func,
+        x0,
+        args=(ansatz_isa, hamiltonian_isa, estimator),
+        method="cobyla",
+        options=options
+    )
+
+# Cell
+res
+
+# Cell
+all(cost_history_dict["prev_vector"] == res.x)
+
+# Cell
+cost_history_dict["iters"] == res.nfev
+
+# Cell
+fig, ax = plt.subplots()
+ax.plot(range(cost_history_dict["iters"]), cost_history_dict["cost_history"])
+ax.set_xlabel("Iterations")
+ax.set_ylabel("Cost")
+plt.draw()
+
+# Cell
+import qiskit_ibm_runtime
+
+qiskit_ibm_runtime.version.get_version_info()
+
+# Cell
+import qiskit
+
+qiskit.version.get_version_info()
+
